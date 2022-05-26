84e78a488ce0feac924b946dce7cc2d525b42e41
Shawna Cooper <you@example.com>
2022-05-25 21:21:56 -0400

    (working endpoint for detail)

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from api import views as api_views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('habits', api_views.HabitListView.as_view(), name='habit-list-api')
+    path('habits', api_views.HabitListView.as_view(), name='habit-list-api'),
+    path('habits/<int:pk>/', api_views.HabitDetailView.as_view(), name='habit-detail-api')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from habit.models import Habit
-from api.serializers import HabitSerializer
+from .serializers import HabitSerializer
+from rest_framework import generics
 
 class HabitListView(APIView):
 #   return JSON listof habit
@@ -11,3 +12,10 @@ class HabitListView(APIView):
         habits     = Habit.objects.filter(user=request.user) 
         serializer = HabitSerializer(habits, many=True)
         return Response(serializer.data)
+
+
+class HabitDetailView(generics.RetrieveDestroyAPIView):
+    queryset          = Habit.objects.all()
+    serializer_class  = HabitSerializer 
+
+
